00fcb130e275b33b14e0edeaf7491e099468c144
beto-machado <luisrobertobeto3@gmail.com>
2022-02-25 14:29:50 -0300

    validando atributo no model proposta

--- app/models/proposta.rb
+++ app/models/proposta.rb
@@ -1,2 +1,14 @@
 class Proposta < ApplicationRecord
+
+  with_options presence: true do
+    validates :nome, uniqueness: true
+    validates :descricao
+    validates :valor
+    validates :inicio
+    validates :fim
+  end
+
+  validates :descricao, length: { maximum: 1000 }
+  validates :valor, numericality: { :greater_than }
+
 end
